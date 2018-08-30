11032339f8e8dc26f287c81dffba2c5296ce8bec
Jonathan Robertson <42648270+JonRobertson93@users.noreply.github.com>
2018-08-30 11:46:35 -0500

    Update file and rename to main.js
    Prevents card from matching with itself.
    Prevents cards from being clicked while 2 cards' animations are running.
    Adds styling to matches and mismatches.

--- js/main.js
+++ js/main.js
@@ -9,10 +9,9 @@ let openCardsList = [];
 let matches = [];
 let movesCounter = 0;
 
-
+	//Game Start
 shuffle(cardNames);
 createCards();
-
 
 
 	// Shuffle function from http://stackoverflow.com/a/2450976
@@ -45,9 +44,8 @@ function createCards(){
 
 
 function openCard(e) {	//(e) is the element that was clicked - a card tile
-	e.target.classList.add('show', 'open', 'bounceIn');		// adding these 2 classes to display the "front" of the clicked card
+	e.target.classList.add('show', 'open', 'bounceIn');		// adding these 3 classes to display the "front" of the clicked card & animate the flip
 	openCardsList.push(e.target);
-
 	checkForMatch(e);
 }
 
@@ -55,65 +53,96 @@ function checkForMatch(e) {
 		//Comparing 2 open cards for equality
 	if (currentPairList.length < 2) {		// if there are only 0 or 1 cards in this list...
 		currentPairList.push(e.target.className);		// adds element class names to the array as strings
+		e.target.removeEventListener('click', openCard); 	//Prevents the card from matching itself
 	}
 
 	if (currentPairList.length === 2) {
-			//remove the "click" Event Listener from ALL CARDS
-			for (let i=0; i<cardButtons.length; i++) {
+		//REMOVE EVENT LISTENER SO CARD CANNOT BE CLICKED WHILE ANIMATIONS ARE RUNNING
+		for (let i=0; i<cardButtons.length; i++) {
 				cardButtons[i].removeEventListener('click', openCard);
 			}
 
-			if (currentPairList[0] === currentPairList[1]) { 		// if all class names of BOTH elements are identical...
-				pairMatches();
+		if (currentPairList[0] === currentPairList[1]) { 		// if all class names of BOTH elements are identical...
+			pairMatches();
 
-			} else {		// if the cards don't match...
-				pairDoesNotMatch();
-			}
-			increaseMoves();
+
+		} else {		// if the cards don't match...
+			pairDoesNotMatch();
+		}
+		increaseMoves();
+
 		}
 	}	
 
 function pairMatches() {
-	console.log('That is a match!');		// We have a match!
-	for (let i=0; i<cardButtons.length; i++) {
-				cardButtons[i].addEventListener('click', openCard);
-			}
-	openCardsList[0].classList.add('match');
-					// Add this match to the list of matched cards.
+	console.log('That is a match!');		// We have a match!	
 	matches.push(openCardsList[0]);
 	matches.push(openCardsList[1]);
+	matchAnimation();	
+}
 
-		
-		// Check if there are 16 cards in the matched array
+function matchAnimation() {
+	openCardsList[0].removeEventListener('click', openCard);	//cannot click same card twice
+	openCardsList[1].removeEventListener('click', openCard);	//cannot click same card twice
+	setTimeout(function() {
+		openCardsList[0].classList.remove('bounceIn');
+		openCardsList[0].classList.add('match', 'animated', 'bounce');
+		openCardsList[1].classList.remove('bounceIn');
+		openCardsList[1].classList.add('match', 'animated','bounce');
+			// Allow the cards to be clicked again
+		for (let i=0; i<cardButtons.length; i++) {
+				cardButtons[i].addEventListener('click', openCard);
+			}
+			// Lock any matched cards
+		for (let i=0; i<matches.length; i++) {
+				matches[i].removeEventListener('click', openCard);
+			}
+		currentPairList = [];
+		openCardsList = [];
+		checkForWin();
+		}, 1000);
+}
+
+function checkForWin(){
 	if (matches.length === 16) {
 			winGame();
 	}
-		// reset our two working lists
-	currentPairList = [];
-	openCardsList = [];
 }
 
 function pairDoesNotMatch() {
 	console.log(`Those cards don't match silly!`);
-		//remove bounceIn class so they will bounce again next time.
-		// FLIP BACK OVER
+	animateMismatch();
 	closeCards();				
+}
+
+function animateMismatch() {
+	setTimeout(function() {
+		openCardsList[0].classList.remove('bounceIn');
+		openCardsList[1].classList.remove('bounceIn');
+		openCardsList[0].classList.add('notMatch', 'animated', 'wobble');
+		openCardsList[1].classList.add('notMatch', 'animated', 'wobble');
+	}, 1000);
+		
 }
 
 
 function closeCards() {	
-		setTimeout(function() {
 				// Hide the cards
-			openCardsList[0].classList.remove('show', 'open', 'bounceIn');
-			openCardsList[1].classList.remove('show', 'open', 'bounceIn');	
+			setTimeout(function() {
+			openCardsList[0].classList.remove('show', 'open','notMatch', 'animated', 'wobble');
+			openCardsList[1].classList.remove('show', 'open','notMatch', 'animated', 'wobble');	
 				// Allow the cards to be clicked again
 			for (let i=0; i<cardButtons.length; i++) {
 				cardButtons[i].addEventListener('click', openCard);
 			}
+			// Lock any matched cards
+			for (let i=0; i<matches.length; i++) {
+				matches[i].removeEventListener('click', openCard);
+			}
 				// Clear the working lists
 			openCardsList = [];	
 			currentPairList = [];
-			}, 1500);
+			}, 2000);
 			
 }
 
@@ -137,13 +166,13 @@ function increaseMoves() {
 	} else {
 		document.getElementById('moveOrMoves').innerHTML = "Moves";
 	}
-	if (movesCounter === 12) {
-		document.querySelector('.fa-star').remove();
-	}
 	if (movesCounter === 15) {
 		document.querySelector('.fa-star').remove();
 	}
 	if (movesCounter === 18) {
+		document.querySelector('.fa-star').remove();
+	}
+	if (movesCounter === 2) {
 		document.querySelector('.fa-star').remove();
 		gameOver();
 	}
@@ -151,11 +180,14 @@ function increaseMoves() {
 
 
 function gameOver() {
-	//alert that you lost the game & "Play Again?" button
+	setTimeout(function(){
+		//alert that you lost the game & "Play Again?" button
 	let playAgain = confirm("GAME OVER! Would you like to play again?");
 	if (playAgain == true) {
 		refreshPage();
 	} else {
 		window.close();
 	}
+	}, 2000);
+	
 }
